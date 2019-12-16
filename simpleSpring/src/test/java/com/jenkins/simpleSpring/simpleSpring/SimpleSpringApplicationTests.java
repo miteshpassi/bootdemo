5413297b1d1f5ad68bab9e314c3d1e155178337c
miteshpassi <mitesh.passi@gmail.com>
2019-12-16 08:30:01 +0530

    fdsf

---
+++ simpleSpring/src/test/java/com/jenkins/simpleSpring/simpleSpring/SimpleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.simpleSpring.simpleSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
